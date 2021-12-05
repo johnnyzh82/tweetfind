c951ac803f8379997f45ad7d5bc27c48c25e5cca
johnnyzh82 <johnnyzh82@gmail.com>
2021-12-04 19:17:10 -0800

    Syncing UI state with the redux store

--- src/features/numberOfResults/numberOfResults.js
+++ src/features/numberOfResults/numberOfResults.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Flex, Stack, RadioGroup, Radio, Text } from '@chakra-ui/react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { setNumberOfResults } from './numberOfResultsSlice';
 
 export const NumberOfResults = () => {
+    const dispatch = useDispatch();
+    const numberOfResult = useSelector((state) => state.numberOfResults);
+
+    const onChange = (val) => {
+        dispatch(setNumberOfResults(val));
+    };
+    
     return (
         <Flex my={4} justifyContent="center">
             <Text mr={3}>Number of results</Text>
-            <RadioGroup defaultValue={10}>
+            <RadioGroup defaultValue={10} value={numberOfResult} onChange={onChange}>
                 <Stack spacing={4} direction="row">
                     <Radio value="10">10 tweets</Radio>
                     <Radio value="15">15 tweets</Radio>
